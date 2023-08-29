03691e97c482e69f6f529e9ab1c4905d6331e643
jg0briel <120822612+jg0briel@users.noreply.github.com>
2023-08-29 11:24:26 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,31 @@
+#1)
+# num1 = int(input("Digite um número:"))
+# num2 = num1 + 1
+# num3 = num1 - 1
+# print("O número colocado foi:{num1}, o seu sucessor é:{num2} e o seu antecessor é:{num3}")
+
+#2)
+# n1 = float(input("Digite um número:"))
+# n2 = float(input("Digite outro número:"))
+
+# ad = n1 + n2
+# sub = n1 - n2
+# mul = n1 * n2
+# div = n1 / n2
+
+# print("A adição dos dois números é:", ad, ", a subtração dos dois números é:", sub, ", a multiplicação dos dois números é:", mul, "e a divisão dos dois números é:", div,)
+
+#3)
+# n1 = float(input("Digite um número:"))
+# n2 = float(input("Digite outro número:"))
+
+# if n1 > n2:
+#     print(True)
+# else:
+#     print(False)
+
+#4)
+# c = float(input("Digite um número:"))
+
+# f = (c*1.8)+32
+# print("A temperatura em fahrenheit é:", f)
